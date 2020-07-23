f592b0e1883f124206983ea515c5cbc21f530fd9
Leah Rumancik <lrumancik@google.com>
2020-07-23 15:37:09 +0000

    bpf: fill out io_filter_func_proto

--- block/bpf_io_filter.c
+++ block/bpf_io_filter.c
@@ -17,10 +17,6 @@
 #define io_filter_rcu_dereference_progs(disk)	\
 	rcu_dereference_protected(disk->progs, lockdep_is_held(&disk->io_filter_lock))
 
-/*
-Need to build this out such that all, but only, necessary functions are
-allowed.
-
 static const struct bpf_func_proto *
 io_filter_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
 {
@@ -37,73 +33,21 @@ io_filter_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
 		return &bpf_map_pop_elem_proto;
 	case BPF_FUNC_map_peek_elem:
 		return &bpf_map_peek_elem_proto;
-	case BPF_FUNC_ktime_get_ns:
-		return &bpf_ktime_get_ns_proto;
-	case BPF_FUNC_tail_call:
-		return &bpf_tail_call_proto;
-	case BPF_FUNC_get_current_pid_tgid:
-		return &bpf_get_current_pid_tgid_proto;
-	case BPF_FUNC_get_current_task:
-		return &bpf_get_current_task_proto;
-	case BPF_FUNC_get_current_uid_gid:
-		return &bpf_get_current_uid_gid_proto;
-	case BPF_FUNC_get_current_comm:
-		return &bpf_get_current_comm_proto;
 	case BPF_FUNC_trace_printk:
-		return bpf_get_trace_printk_proto();
-	case BPF_FUNC_get_smp_processor_id:
-		return &bpf_get_smp_processor_id_proto;
-	case BPF_FUNC_get_numa_node_id:
-		return &bpf_get_numa_node_id_proto;
-	case BPF_FUNC_perf_event_read:
-		return &bpf_perf_event_read_proto;
-	case BPF_FUNC_probe_write_user:
-		return bpf_get_probe_write_proto();
-	case BPF_FUNC_current_task_under_cgroup:
-		return &bpf_current_task_under_cgroup_proto;
-	case BPF_FUNC_get_prandom_u32:
-		return &bpf_get_prandom_u32_proto;
-	case BPF_FUNC_probe_read_user:
-		return &bpf_probe_read_user_proto;
-	case BPF_FUNC_probe_read_kernel:
-		return &bpf_probe_read_kernel_proto;
-	case BPF_FUNC_probe_read:
-		return &bpf_probe_read_compat_proto;
-	case BPF_FUNC_probe_read_user_str:
-		return &bpf_probe_read_user_str_proto;
-	case BPF_FUNC_probe_read_kernel_str:
-		return &bpf_probe_read_kernel_str_proto;
-	case BPF_FUNC_probe_read_str:
-		return &bpf_probe_read_compat_str_proto;
-#ifdef CONFIG_CGROUPS
-	case BPF_FUNC_get_current_cgroup_id:
-		return &bpf_get_current_cgroup_id_proto;
-#endif
-	case BPF_FUNC_send_signal:
-		return &bpf_send_signal_proto;
-	case BPF_FUNC_perf_event_output:
-		return &bpf_perf_event_output_proto;
-	case BPF_FUNC_get_stackid:
-		return &bpf_get_stackid_proto;
-	case BPF_FUNC_get_stack:
-		return &bpf_get_stack_proto;
-	case BPF_FUNC_perf_event_read_value:
-		return &bpf_perf_event_read_value_proto;
-#ifdef CONFIG_BPF_KPROBE_OVERRIDE
-	case BPF_FUNC_override_return:
-		return &bpf_override_return_proto;
-#endif
+		if (capable(CAP_SYS_ADMIN))
+			return bpf_get_trace_printk_proto();
+		/* else fall through */
 	default:
 		return NULL;
 	}
 }
-*/
+
 
 const struct bpf_prog_ops io_filter_prog_ops = {
 };
 
 const struct bpf_verifier_ops io_filter_verifier_ops = {
-	.get_func_proto = bpf_tracing_func_proto,
+	.get_func_proto = io_filter_func_proto,
 	.is_valid_access = btf_ctx_access,
 };
 
